ecb3aa62e0f2c8a281ef58bad0cfae0ca7b72302
João <joaog.alves14@gmail.com>
2024-03-18 09:54:24 -0300

    state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState("dev");
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <Employee name="Caleb" role="Intern"/>
-          <Employee name="Abby"/>
-          <Employee name="Jon"/>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="Caleb" role="Intern" />
+          <Employee name="Abby" role={role} />
+          <Employee name="Jon" />
           <Employee />
         </>
       ) : (
