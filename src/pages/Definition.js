98b4e49dbaa54942e9e651d0fc3bf1bea38637a6
João <joaog.alves14@gmail.com>
2024-03-21 08:27:01 -0300

    style the dictionary

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import NotFound from '../components/NotFound'
+import NotFound from "../components/NotFound";
+import DefinitionSearch from "../components/DefinitionSearch";
 
 export default function Definition() {
   const [word, setWord] = useState();
-  const [notFound, setNotFound] = useState(false)
+  const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
+
   let { search } = useParams();
   const navigate = useNavigate();
 
@@ -13,17 +16,41 @@ export default function Definition() {
     fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + search)
       .then((response) => {
         if (response.status === 404) {
-          setNotFound(true)
+          setNotFound(true);
+        } else if (response.status === 401) {
+          navigate("/login");
+        }
+        if (!response.ok) {
+          setError(true);
+
+          throw new Error("Something went wrong");
         }
         return response.json();
       })
+
       .then((data) => {
         setWord(data[0].meanings);
-      });
+      })
+      .catch((e) => {
+        console.log(e.message)
+      })
   }, []);
 
   if (notFound === true) {
-    return <NotFound />
+    return (
+      <>
+        <NotFound />
+        <Link to="/dictionar">Search another</Link>
+      </>
+    );
+  }
+  if (error === true) {
+    return (
+      <>
+        <p>Something went wrong, try again</p>
+        <Link to="/dictionar">Search another</Link>
+      </>
+    );
   }
   return (
     <>
@@ -38,6 +65,8 @@ export default function Definition() {
               </p>
             );
           })}
+          <p>Search again:</p>
+          <DefinitionSearch />
         </>
       ) : null}
     </>
