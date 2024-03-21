8c6366bb657f164996705984a4c2031ce2ede12b
João <joaog.alves14@gmail.com>
2024-03-21 11:10:38 -0300

    added individual customer page

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,31 @@
-export default function Customers(){
-  return <h1>Hello there!</h1>
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { baseUrl } from "../shared";
+
+export default function Customers() {
+  const [customers, setCustomers] = useState();
+  useEffect(() => {
+    const url = baseUrl + "api/customers/";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCustomers(data.customers);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Here are our customers:</h1>
+      {customers
+        ? customers.map((customer) => {
+            return (
+              <p>
+                <Link to={"/customers/" + customer.id}>{customer.name}</Link>
+              </p>
+            );
+          })
+        : ""}
+    </>
+  );
 }
